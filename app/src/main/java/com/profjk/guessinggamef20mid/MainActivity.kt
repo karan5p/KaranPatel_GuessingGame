64faac17ae1296969e1d1c1352bedd756fd1b21b
Karan <pate1228@sheridancollege.ca>
2020-10-22 09:48:04 -0400

    Karan Patel 991519115 Added validation to check if field is empty

--- app/src/main/java/com/profjk/guessinggamef20mid/MainActivity.kt
+++ app/src/main/java/com/profjk/guessinggamef20mid/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +17,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
     }
+    fun onClick(v: View?){
+        if(v != null){
+            when(v.id){
+                btnCheck.id->{
+                    if(this.validateInfo()){
+                        this.getGuess()
+                    }
+                }
+            }
+        }
+    }
 
+    fun validateInfo() : Boolean{
+        if(edtAnswer.text.toString().isEmpty()){
+            edtAnswer.error = "Please enter your guess!"
+            return false
+        }
+        return true
+    }
+
+    fun getGuess(){
+
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_home, menu)
         return super.onCreateOptionsMenu(menu)
